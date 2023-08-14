6076302f292a6459781a97f3f9e3b7a2296273da
Pedro Gabriel <pedrpg5@gmail.com>
2023-08-14 08:24:15 -0300

    Atualizando

--- api/routes/web.php
+++ api/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Ratchet\Server\IoServer;
-use App\Http\WebSocketServer;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,13 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/websocket', function () {
-    $server = IoServer::factory(
-        new WebSocketServer(),
-        8080 // Porta para o WebSocket
-    );
-
-    $server->run();
 });
